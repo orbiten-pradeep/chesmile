3a52a1e30c7e90ad9c098ea6a0934644c5f8cbcc
orbiten-pradeep <pradeep.j@orbiten.in>
2017-04-14 22:31:23 +0530

    Galaries Page updated
    Galaries Page updated

--- src/Controller/GalariesController.php
+++ src/Controller/GalariesController.php
@@ -53,14 +53,59 @@ class GalariesController extends AppController
     {
         $galary = $this->Galaries->newEntity();
         if ($this->request->is('post')) {
-            $galary = $this->Galaries->patchEntity($galary, $this->request->data);
-            if ($this->Galaries->save($galary)) {
-                $this->Flash->success(__('The galary has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The galary could not be saved. Please, try again.'));
+            /////////////////////////////////////////////////////////////////////////
+            $galary_file = "";
+            if(!empty($this->request->data['galary']))
+            {
+                $error=array();
+                $extension=array("jpeg","jpg","png","gif");
+                foreach($this->request->data['galary'] as $key=>$tmp_name)
+                {
+                    $file_name=$this->request->data['galary'][$key]["name"];
+                    $file_tmp=$this->request->data['galary'][$key]["tmp_name"];
+                    $ext=pathinfo($file_name,PATHINFO_EXTENSION);
+                    if(in_array($ext,$extension))
+                    {
+                        $uploadFolder = WWW_ROOT . 'img/Galary';
+                        if( !file_exists($uploadFolder) ){
+                            mkdir($uploadFolder);
+                        }
+                        
+                        
+                        $filename = str_replace(" ", "-", rand(1, 3000) . $file_name);
+                        //move_uploaded_file($file_tmp=$this->request->data['Mediapartners'][$key]["tmp_name"], WWW_ROOT . 'img/Mediapartners' . DS . $filename);
+                        if(!file_exists(WWW_ROOT . 'img/Galary' . DS . $filename))
+                        {
+                            move_uploaded_file($file_tmp=$this->request->data['galary'][$key]["tmp_name"], WWW_ROOT . 'img/Galary' . DS . $filename);
+                            $this->request->data['galary'][$key]["name"] = $filename;
+                        }
+                        else
+                        {
+                            $filename=basename($file_name,$ext);
+                            $newFileName=$filename.time().".".$ext;
+                            move_uploaded_file($file_tmp=$this->request->data['galary'][$key]["tmp_name"], WWW_ROOT . 'img/Galary' . DS . $newFileName);
+                            $this->request->data['galary'][$key]["name"] = $newFileName;
+                        }
+                    }
+                    else
+                    {
+                        array_push($error,"$file_name, ");
+                    }
+                    $galary_file[$key] =  $filename;
+                }
             }
+            $this->request->data['galary'] = $galary_file;
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+            $events_id_new = $this->request->data['events_id'];
+            foreach ($galary_file as $key => $value) {
+                # code...
+                $galary = $this->Galaries->newEntity();
+                $galary['events_id'] = $events_id_new;
+                $galary['galary'] = $value;
+                $this->Galaries->save($galary);
+            }
+            return $this->redirect(['action' => 'index']);
+
         }
         $events = $this->Galaries->Events->find('list', ['limit' => 200]);
         $this->set(compact('galary', 'events'));
