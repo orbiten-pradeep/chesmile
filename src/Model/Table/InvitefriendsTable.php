a80e564173aa63d932657227ddcfec025f1fde2e
orbiten-pradeep <pradeep.j@orbiten.in>
2017-04-19 23:22:33 +0530

    Galaries and Email Added for Event Detail Page
    Galaries and Email Added for Event Detail Page

---
+++ src/Model/Table/InvitefriendsTable.php
@@ -0,0 +1,83 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Invitefriends Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $Events
+ *
+ * @method \App\Model\Entity\Invitefriend get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Invitefriend newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Invitefriend[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Invitefriend|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Invitefriend patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Invitefriend[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Invitefriend findOrCreate($search, callable $callback = null)
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class InvitefriendsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('invitefriends');
+        $this->displayField('ID');
+        $this->primaryKey('ID');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Events', [
+            'foreignKey' => 'events_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('ID')
+            ->allowEmpty('ID', 'create');
+
+        $validator
+            ->email('email')
+            ->requirePresence('email', 'create')
+            ->notEmpty('email');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['email']));
+        $rules->add($rules->existsIn(['events_id'], 'Events'));
+
+        return $rules;
+    }
+}
