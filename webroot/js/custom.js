75383e56b2853e94a7115f447e0ba0a88a391f74
vasanth1407 <vasanth@orbiten.in>
2017-05-10 18:59:27 +0530

    minor changes

--- webroot/js/custom.js
+++ webroot/js/custom.js
@@ -803,12 +803,12 @@ else if(ePage == 'view') {
 	  collapsedHeight: 100
 	});*/
 
-	$('#eventDescription').slimScroll({ 
-		height: '150px', 
-		allowPageScroll: true,
-		touchScrollStep: 10,
-		wheelStep : 50,
-  	});
+	// $('#eventDescription').slimScroll({ 
+	// 	height: '150px', 
+	// 	allowPageScroll: true,
+	// 	touchScrollStep: 10,
+	// 	wheelStep : 50,
+ //  	});
 
 	function validateEmail(email) {
 	    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
