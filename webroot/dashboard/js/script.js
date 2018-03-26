64cb27d31ca3a9cf634c35ab8ca86943d9f9e36b
RosalRoslin <31982792+RosalRoslin@users.noreply.github.com>
2018-03-26 16:57:13 +0530

    added script file for admin panel

--- webroot/dashboard/js/script.js
+++ webroot/dashboard/js/script.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(document).ready(function() {
+	//alert(1);
+	$('.radio-select-list').change(function(){
+		//alert(2);
+		$('a#deactivate').attr('href',  $(this).attr('deactivate-href-url')); // better if you add an id to anchor & then
+		$('a#activate').attr('href', $(this).attr('activate-href-url')); // better if you add an id to anchor & then
+		$('a#view').attr('href', $(this).attr('view-href-url')); // better if you add an id to anchor & then
+		$('a#edit').attr('href',  $(this).attr('edit-href-url')); // better if you add an id to anchor & then 
+		$('a#add').attr('href',  $(this).attr('add-href-url')); // better if you add an id to anchor & then $('a#idofAnchor')
+	});
+});
