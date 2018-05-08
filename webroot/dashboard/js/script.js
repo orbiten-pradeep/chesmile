42eec38a7d96aa0364d0d8f3eeaa168d3e5ffc9a
RosalRoslin <31982792+RosalRoslin@users.noreply.github.com>
2018-05-08 13:32:47 +0530

    updated script file for admin panel

--- webroot/dashboard/js/script.js
+++ webroot/dashboard/js/script.js
@@ -2,10 +2,13 @@
 	//alert(1);
 	$('.radio-select-list').change(function(){
 		//alert(2);
+		$('a#chennai').attr('href', $(this).attr('chennai-href-url')); // better if you add an id to anchor & then
+		$('a#reply').attr('href',  $(this).attr('reply-href-url')); // better if you add an id to anchor & then
 		$('a#deactivate').attr('href',  $(this).attr('deactivate-href-url')); // better if you add an id to anchor & then
 		$('a#activate').attr('href', $(this).attr('activate-href-url')); // better if you add an id to anchor & then
 		$('a#view').attr('href', $(this).attr('view-href-url')); // better if you add an id to anchor & then
 		$('a#edit').attr('href',  $(this).attr('edit-href-url')); // better if you add an id to anchor & then 
-		$('a#add').attr('href',  $(this).attr('add-href-url')); // better if you add an id to anchor & then $('a#idofAnchor')
+		$('a#admin').attr('href',  $(this).attr('admin-href-url'));
+		// $('a#add').attr('href',  $(this).attr('add-href-url')); // better if you add an id to anchor & then $('a#idofAnchor')
 	});
 });
