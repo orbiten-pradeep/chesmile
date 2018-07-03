dab887227e83fb986f872fc5f25957fd8e2dc060
RosalRoslin <31982792+RosalRoslin@users.noreply.github.com>
2018-07-03 17:25:45 +0530

    Update banners.sql

--- DB schema/banners.sql
+++ DB schema/banners.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS `banners` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `events_id` int(11) NOT NULL,
+  `bannerstype` varchar(200) NOT NULL,
+  `categories_id` int(11) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `caption` varchar(255) DEFAULT NULL,
   `active` int(11) DEFAULT '0',
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
